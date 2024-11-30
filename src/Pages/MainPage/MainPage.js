4157a3d3e505e872ae2b4ba37b9c06f0acc3eee6
Tomasz2002 <Tomaszov1233@gmail.com>
2024-11-30 17:49:36 +0100

    testing

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -118,7 +118,24 @@ export default function MainPage() {
 
 
   {/* Strzałki - nawigacja */}
-  
+  <button
+    className="carousel-control-prev"
+    type="button"
+    data-bs-target="#mainSlider"
+    data-bs-slide="prev"
+  >
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button
+    className="carousel-control-next"
+    type="button"
+    data-bs-target="#mainSlider"
+    data-bs-slide="next"
+  >
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
 </div>
 
 
